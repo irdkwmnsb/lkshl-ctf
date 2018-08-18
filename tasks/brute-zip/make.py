f068d72c7e31a8b720b2fb3418a422609efb168f
Maxim <supermax74.02@gmail.com>
2018-08-18 13:06:42 +0500

    WARMUP 10, FOR - Готово

---
+++ tasks/brute-zip/make.py
@@ -0,0 +1,63 @@
+import argparse
+import base64
+import os
+from itertools import count
+from typing import Dict, Tuple
+
+def sh(*args, sudo=False, env=True, fatal=True):
+    import subprocess as sp
+    if sudo:
+        print('Requesting sudo privileges for command:')
+        print('  ' + ' '.join(args))
+        args = ['sudo', *args]
+    if env:
+        args = ['/usr/bin/env', *args]
+    ret = sp.call(args)
+    if ret != 0:
+        if fatal:
+            print('Command failed (return status {}):'.format(ret))
+            print('  ' + ' '.join(args))
+            raise OSError('Command failed (return status {})'.format(ret))
+        else:
+            return False
+    return True
+
+def make_task(flag: str) -> str:
+    password = "9258"
+    with open('flag.txt', 'w+') as f:
+        f.write(flag)
+    sh('zip', '--password', password, 'task.zip', 'flag.txt')
+    with open('task.zip', 'rb') as f:
+        out = base64.standard_b64encode(f.read()).decode()
+    sh('rm', 'task.zip')
+    sh('rm', 'flag.txt')
+    return out
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Make dem tasks")
+    parser.add_argument('where', help = "Where would you like task files outputted to")
+    flag_group = parser.add_mutually_exclusive_group()
+    flag_group.add_argument('--flags', help = "File to take flags from", type=list)
+    flag_group.add_argument('--flag', help = "Single flag to be hidden")
+    parser.add_argument('--v', help = "Print made tasks", action = "store_true")
+    g = parser.parse_args()
+    output = dict() # type: Dict[str, Tuple[str, str]]
+    if g.flag:
+        output[0] = make_task(g.flag)
+    elif g.flags:
+        for i, flag in zip(count(), g.flags):
+            output[i] = make_task(flag)
+    else:
+        from flags_and_teams import data
+        for team_id, flag in data.items():
+            output[team_id] = make_task(flag)
+
+    if g.v:
+        print(output)
+    else:
+        for team_id, flag in output.items():
+            with open(g.where, 'w+') as f:
+                f.write("tasks = ")
+                f.write(repr(list(output.values())))
+                f.write("\nanswers = ")
+                f.write(repr(list(output.keys())))
