0160b1b3f22235e4dbc0a4892e008e76436d9d59
Alexander Korzun <korzun.sas@mail.ru>
2018-08-19 15:56:03 +0300

    Fixed gen.py in web tasks

--- tasks/code-lock/gen.py
+++ tasks/code-lock/gen.py
@@ -25,24 +25,34 @@ def sh(*args, sudo=False, env=True, fatal=True):
             return False
     return True
 
-def get_team_num(n):
+def get_team_id(n):
     with open('team_ids.txt') as f:
         return f.read().split('\n')[n-1]
 
 def main():
     if len(sys.argv) < 2:
-        print('Usage: gen.py <team_num>')
+        print('Usage: gen.py <NUM_TEAMS>')
         return
 
     # Prepare magic and flag
-    team_num = int(sys.argv[1])
-    flag = teams_and_flags.data[get_team_num(team_num)]
+    n = int(sys.argv[1])
+    for i in range(n):
+        team_id = get_team_id(i + 1)
+        print('({}/{}) Generating subdir for team with id {}'.format(i + 1, n, team_id))
+        flag = teams_and_flags.data[team_id]
 
-    sh('cp', '-r', 'wsroot', 'team' + str(team_num))
-    sh('sed', '-i', 's/@@_FLAG_@@/{}/g'.format(flag), 'team{}/flag.php'.format(team_num))
-    sh('cp', 'lighttpd.conf', 'lighty_team{}.conf'.format(team_num))
-    sh('sed', '-i', 's/@@_PORT_@@/{}/g'.format(BASE_PORT + team_num), 'lighty_team{}.conf'.format(team_num))
-    sh('sed', '-i', 's#@@_ROOT_@@#{}#g'.format(os.path.join(os.getcwd(), 'team' + str(team_num))), 'lighty_team{}.conf'.format(team_num))
+        try:
+            os.stat(team_id)
+            sh('rm', '-rf', team_id)
+        except FileNotFoundError:
+            pass
+        sh('cp', '-r', 'wsroot', team_id)
+        sh('sed', '-i', 's/@@_FLAG_@@/{}/g'.format(flag), team_id + '/flag.php')
+        
+        # Пофиг на это, мы не поднимаем N инстансов lighttpd, обойдёмся одним nginx'ом
+        # sh('cp', 'lighttpd.conf', 'lighty_team{}.conf'.format(team_num))
+        # sh('sed', '-i', 's/@@_PORT_@@/{}/g'.format(BASE_PORT + team_num), 'lighty_team{}.conf'.format(team_num))
+        # sh('sed', '-i', 's#@@_ROOT_@@#{}#g'.format(os.path.join(os.getcwd(), 'team' + str(team_num))), 'lighty_team{}.conf'.format(team_num))
 
     print('Done')
 
---
+++ tasks/code-lock/team_4_SrutkQgo/flag.php
@@ -0,0 +1,3 @@
+<?php
+$flag = "LKL{MD5_0F_TRRU3_1N_C0OK1E5_78tc1j73JK}";
+?>
